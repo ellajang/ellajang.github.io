f2353e1f670f5b2d13a1f02bb8d6f89f5c2edd04
jungeun-jang <3lla.jang@gmail.com>
2023-06-17 22:41:31 +0900

    Updates

---
+++ component---src-pages-memoir-tsx-6e5d630b917aa4f367e0.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[374],{8864:function(t,u,e){e.r(u),e.d(u,{default:function(){return s}});var n=e(3829),r=e(7294),a=e(917);function s(){return(0,a.jsx)(r.Fragment,null,(0,a.jsx)(n.Z,null))}}}]);
+//# sourceMappingURL=component---src-pages-memoir-tsx-6e5d630b917aa4f367e0.js.map
