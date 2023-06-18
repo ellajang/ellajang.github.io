5561eeedf20a3e8fe6d55cd9f2d110cb29cc77cf
jungeun-jang <3lla.jang@gmail.com>
2023-06-18 23:23:46 +0900

    Template

--- src/pages/basicResource/CS.tsx
+++ src/pages/basicResource/CS.tsx
@@ -6,6 +6,7 @@ export default function CS() {
   return (
     <>
       <Header />
+      <BlogListTemplate />
     </>
   )
 }
--- src/templates/blog-list-template.tsx
+++ src/templates/blog-list-template.tsx
@@ -1,57 +1,95 @@
+import PostHead from 'components/Post/PostHead'
+import Template from 'components/Common/Template'
+import { graphql } from 'gatsby'
 import React from 'react'
-import styled from 'styled-components'
+import { PostFrontmatterType } from 'types/PostItem.types'
+import PostContent from 'components/Post/PostContent'
+import CommentWidget from 'components/Post/CommentWidget'
+import { ThemeContextProvider } from 'hooks/Theme'
 
-const Post = styled.div`
-  display: flex;
-  justify-content: space-between;
-  border-bottom: 1px solid gray;
-  padding: 20px 0;
-
-  h2 {
-    margin: 0;
+type PostTemplateProps = {
+  data: {
+    allMarkdownRemark: {
+      edges: PostPageItemType[]
+    }
   }
-
-  img {
-    width: 100px;
-    height: 100px;
+  location: {
+    href: string
   }
+}
 
-  p {
-    color: gray;
+export type PostPageItemType = {
+  node: {
+    html: string
+    frontmatter: PostFrontmatterType
   }
-`
+}
 
-const BlogListTemplate = () => {
-  const posts = [
-    {
-      title: '첫 번째 포스트',
-      summary: '첫 번째 포스트에 대한 요약...',
-      thumbnail: 'https://placekitten.com/200/200',
-      date: '2023-06-17',
+const BlogListTemplate: React.FC<PostTemplateProps> = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+  location: { href },
+}) => {
+  const {
+    node: {
+      html,
+      frontmatter: {
+        title,
+        date,
+        summary,
+        categories,
+        thumbnail: {
+          childImageSharp: { gatsbyImageData },
+          publicUrl,
+        },
+      },
     },
-    {
-      title: '두 번째 포스트',
-      summary: '두 번째 포스트에 대한 요약...',
-      thumbnail: 'https://placekitten.com/200/200',
-      date: '2023-06-18',
-    },
-    // 추가로 다른 포스트들...
-  ]
+  } = edges[0]
 
   return (
-    <div>
-      {posts.map((post, index) => (
-        <Post key={index}>
-          <div>
-            <h2>{post.title}</h2>
-            <p>{post.summary}</p>
-            <p>{post.date}</p>
-          </div>
-          <img src={post.thumbnail} alt={post.title} />
-        </Post>
-      ))}
-    </div>
+    <ThemeContextProvider>
+      <Template
+        title={title}
+        description={summary}
+        url={href}
+        image={publicUrl}
+      >
+        <PostHead
+          title={title}
+          date={date}
+          categories={categories}
+          thumbnail={gatsbyImageData}
+        />
+        <PostContent html={html} />
+        <CommentWidget />
+      </Template>
+    </ThemeContextProvider>
   )
 }
 
 export default BlogListTemplate
+
+export const queryMarkdownDataBySlug = graphql`
+  query queryMarkdownDataBySlug($slug: String) {
+    allMarkdownRemark(filter: { fields: { slug: { eq: $slug } } }) {
+      edges {
+        node {
+          html
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              childImageSharp {
+                gatsbyImageData
+              }
+              publicURL
+            }
+          }
+        }
+      }
+    }
+  }
+`
