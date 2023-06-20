f7c711c6e4c4165d93787ba47dc88c24406c8502
jungeun-jang <3lla.jang@gmail.com>
2023-06-20 11:44:26 +0900

    fix

--- src/components/CategoryPage/Title.tsx
+++ src/components/CategoryPage/Title.tsx
@@ -9,11 +9,11 @@ type TitleProps = {
 const Title: React.FC<TitleProps> = ({ titleText }) => {
   const { theme } = useContext(ThemeContext)
   return (
-    <Wrapper>
-      <Background theme={theme}>
+    <Background theme={theme}>
+      <Wrapper>
         <TitleStyle>{titleText}</TitleStyle>
-      </Background>
-    </Wrapper>
+      </Wrapper>
+    </Background>
   )
 }
 export default Title
@@ -21,18 +21,20 @@ export default Title
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-end;
+  align-items: flex-start;
   width: 100%;
-  margin: 50px 0px 0px -10px;
+  height: 150px;
   @media (max-width: 768px) {
     width: 100%;
-    height: 20px;
-    padding: 26px 0px 80px 0px;
+    height: 120px;
+    padding: 0 20px;
   }
 `
 const Background = styled.div`
-  padding: 50px 10px 30px 10px;
-  width: 100%;
+  box-sizing: border-box;
+  width: 100vw;
+  margin: 2px 0px 0px -8px;
   background-color: ${props =>
     props.theme === 'light' ? '#90a4ae' : '#0b1118'};
   color: #ffffff;
@@ -41,8 +43,9 @@ const Background = styled.div`
 const TitleStyle = styled.div`
   font-size: 30px;
   font-weight: 600;
-  text-align: center;
+  margin: 26px 50px;
   @media (max-width: 768px) {
     font-size: 20px;
+    margin: 22px 20px;
   }
 `
